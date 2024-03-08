5ab4f92eacbfbc9fc00bdc23b4037b6c58576812
akarakosee <132479583+akarakosee@users.noreply.github.com>
2024-03-08 15:40:59 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    //wqşlkweq
+
+    /*
+    wqeweqw
+    qew
+    qew
+    wqe
+    fa
+    s
+    */
+
+
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+
+
+}
